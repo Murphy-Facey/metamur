83fe68c8c17b89991d49f89a5ed4d3c0eab9e723
Murphy-Facey <faceymaxal17@outlook.com>
2022-05-08 20:11:31 -0500

    feat: added header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from "@/modules/common/TheHeader.vue";
+</script>
 
 <template>
-  <h1 class="text-red-500">Welcome to the MetaMur!</h1>
+  <main class="flex flex-col items-center">
+    <the-header />
+    <h1 class="text-red-500">Welcome to the MetaMur!</h1>
+  </main>
 </template>
 
 <style></style>
---
+++ src/modules/common/TheHeader.vue
@@ -0,0 +1,95 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import IconFile from "@/modules/icons/IconFile.vue";
+
+const menuItems = [
+  { name: "home", displayName: "Home" },
+  { name: "about", displayName: "About Me" },
+  { name: "projects", displayName: "Projects/Work" },
+  { name: "contact", displayName: "Contact" },
+];
+
+const selectedMenuItem = ref("home");
+
+const changeMenuItem = (menuItemName: string) => {
+  selectedMenuItem.value = menuItemName;
+};
+</script>
+
+<template>
+  <header class="w-full max-w-[1200px] h-[64px] flex items-center">
+    <div class="flex items-center gap-x-4">
+      <img src="@/assets/logo.svg" alt="logo" class="w-12" />
+      <h2 class="text-2xl text-neutral-500 font-fancy">Murphy D. Facey</h2>
+    </div>
+    <nav class="flex ml-auto items-center">
+      <ul class="flex gap-x-3">
+        <li
+          v-for="(item, index) in menuItems"
+          :key="`menu-items-${index}`"
+          class="menu_items px-2 pt-1 pb-1.5 font-medium rounded-[5px] select-none"
+          :class="{
+            active: item.name === selectedMenuItem,
+          }"
+          @click="changeMenuItem(item.name)"
+        >
+          {{ item.displayName }}
+        </li>
+      </ul>
+      <button
+        class="group flex items-center gap-x-2 ml-3 p-2 border border-neutral-700 hover:border-neutral-400 text-neutral-400 text-sm font-medium rounded-[5px]"
+      >
+        <icon-file
+          class="fill-neutral-700 group-hover:fill-neutral-400 w-3 pb-0.5"
+        />
+        Download Resume
+      </button>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+$border-offset: 10%;
+
+.menu_items {
+  position: relative;
+  transition: color linear 250ms;
+  @apply text-neutral-600;
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: 4px;
+    left: $border-offset;
+    width: 0px;
+    height: 4px;
+    border-radius: 5px;
+
+    @apply bg-sky-500;
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    @apply bg-neutral-700 text-neutral-300;
+
+    &::before {
+      animation: animate-border 250ms forwards;
+    }
+  }
+
+  &.active {
+    @apply bg-neutral-600 text-neutral-300;
+  }
+}
+
+@keyframes animate-border {
+  0% {
+    width: 0;
+  }
+
+  100% {
+    width: calc(100% - ($border-offset * 2));
+  }
+}
+</style>
